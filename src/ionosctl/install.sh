7856e2be76e6249f670096aa6c32f623f559ca77
Marcus Windschuegl <marcus.windschuegl@capgemini.com>
2026-01-23 11:12:55 +0000

    fix: Specified version does not contain v-Prefix

--- src/ionosctl/install.sh
+++ src/ionosctl/install.sh
@@ -33,7 +33,7 @@ ensure_packages curl ca-certificates
 echo "Activating feature 'ionosctl'"
 echo "The provided version is: ${VERSION}"
 
-curl -sL https://github.com/ionos-cloud/ionosctl/releases/download/${VERSION}/ionosctl-${VERSION#v}-linux-amd64.tar.gz | tar -xzv &&\
+curl -sL https://github.com/ionos-cloud/ionosctl/releases/download/v${VERSION#v}/ionosctl-${VERSION#v}-linux-amd64.tar.gz | tar -xzv &&\
   mv ionosctl /usr/local/bin
 
 chmod +x /usr/local/bin/ionosctl
